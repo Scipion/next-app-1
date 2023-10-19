d0553b239b7c68861119a616588912bec2962a60
Scipion <zapador50@gmail.com>
2023-10-19 11:37:03 +0200

    Added babel config with next/babel+macros & fallback for v8

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const withTwin = require("./config/withTwin.js");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withTwin({});
+const nextConfig = withTwin({
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer)
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        v8: require.resolve("v8"),
+      };
+    return config;
+  },
+});
 
 module.exports = nextConfig;
